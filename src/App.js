e8a9be9b31f4076c7b3701573b09b195bbc40b58
Adam McIntosh <adam.mcintosh.10@gmail.com>
2020-12-08 21:34:54 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {{ textAlign: 'center' }}>
+      <h1>DID NC STATE WIN?</h1>
+      <div style={{ fontSize: '120px', color: '#cc0000' }}>YES!</div>
     </div>
+
   );
 }
 
