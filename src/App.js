67b5c74abc3caa8ef981bf592e30e58b0a2851cb
adamuntius <adam.mcintosh.10@gmail.com>
2020-12-08 23:27:25 -0500

    title change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet';
+
+const title = "Did NC State Win?";
 
 function App() {
   return (
-    <div style = {{ textAlign: 'center' }}>
-      <h1>DID NC STATE WIN?</h1>
-      <div style={{ fontSize: '120px', color: '#cc0000' }}>YES!</div>
+    <div>
+      <Helmet>
+        <title>{ title }</title>
+      </Helmet>
+      <div style = {{ textAlign: 'center' }}>
+        <h1>DID NC STATE WIN?</h1>
+        <div style={{ fontSize: '120px', color: '#cc0000' }}>YES!</div>
+      </div>
     </div>
 
   );
